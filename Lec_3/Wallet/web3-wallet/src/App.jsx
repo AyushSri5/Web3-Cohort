65a19ea25570ff18a648bbbb223a6108268b84d2
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-08-22 19:46:08 +0530

    Added send functionality.

--- Lec_3/Wallet/web3-wallet/src/App.jsx
+++ Lec_3/Wallet/web3-wallet/src/App.jsx
@@ -3,10 +3,40 @@ import './App.css'
 import {generateMnemonic} from 'bip39'
 import SolanaWallet from './components/SolanaWallet'
 import EthWallet from './components/EthWallet'
+import { Network, Alchemy, Wallet, Utils } from "alchemy-sdk";
+
 
 function App() {
   const [mnemonic, setMnemonic] = useState("")
   
+  const privateKey = '0xab72008c0fd279d73d983d8d4936c1cacb42bbd7114649b4dbc661980abbbfa8';
+  const settings = {
+    apiKey: "W5c5MuiMAzNX0kEUZpDa4_6VlWxghBfQ",
+    network: Network.ETH_MAINNET, // Replace with your network.
+  };
+  const alchemy = new Alchemy(settings);
+  const wallet = new Wallet(privateKey);
+
+
+async function sendTransaction(){
+  const transaction = {
+    to: "0xa238b6008Bc2FBd9E386A5d4784511980cE504Cd",
+    value: Utils.parseEther("0.001"),
+    gasLimit: "21000",
+    maxPriorityFeePerGas: Utils.parseUnits("5", "gwei"),
+    maxFeePerGas: Utils.parseUnits("20", "gwei"),
+    nonce: await alchemy.core.getTransactionCount(wallet.getAddress()),
+    type: 2,
+    chainId: 1, // Corresponds to ETH_MAINNET
+  };
+const rawTransaction = await wallet.signTransaction(transaction);
+const result=await alchemy.transact.sendTransaction(rawTransaction);
+console.log(result);
+
+}
+
+  sendTransaction();
+
   return (
     <>
       <button onClick={async function() {
