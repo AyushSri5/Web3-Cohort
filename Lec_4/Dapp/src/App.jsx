afa1023339641df009695d734089abba755df4ca
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-09-04 17:16:54 +0530

    Added airdrop and show balance.

--- Lec_4/Dapp/src/App.jsx
+++ Lec_4/Dapp/src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css';
 import './App.css'
+import { RequestAirdrop } from './Airdrop';
+import ShowSolBalance from './ShowSolBalance';
 
 function App() {
 
+  const network = WalletAdapterNetwork.Devnet;
 
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   return (
-    <div>
-     <h1>Hi there</h1>
-    </div>
+    <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div style={{ display: 'flex', justifyContent: "space-between" }}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+          <RequestAirdrop />
+          <ShowSolBalance />
+          {/* <Tokens /> */}
+          {/* <SignMessage /> */}
+          {/* <SendTokens /> */}
+        </WalletModalProvider>
+    </WalletProvider>
+</ConnectionProvider>
   )
 }
 
