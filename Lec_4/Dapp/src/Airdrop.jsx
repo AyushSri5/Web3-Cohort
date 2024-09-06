2cb86222661a66439a9238557c3dc2c79e2d1b12
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-09-06 17:13:18 +0530

    Get user token balances

--- Lec_4/Dapp/src/Airdrop.jsx
+++ Lec_4/Dapp/src/Airdrop.jsx
@@ -1,15 +1,29 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import * as solanaWeb3 from '@solana/web3.js'
 
 
 export function RequestAirdrop(){
     const wallet = useWallet();
     const { connection } = useConnection();
 
+    
+
+    // console.log(LAMPORTS_PER_SOL);
+    
+    async function getTokens(){
+        console.log(wallet.publicKey);
+        
+        const tokens = await connection.getTokenAccountBalance(wallet.publicKey);
+        console.log(tokens);
+    }
+    getTokens();
     async function requestAirdrop() {
         let amount = document.getElementById("amount").value;
         await connection.requestAirdrop(wallet.publicKey, amount * LAMPORTS_PER_SOL);
         alert("Airdropped " + amount + " SOL to " + wallet.publicKey.toBase58());
+    
+        
     }
     return <div>
          <input id="amount" type="text" placeholder="Amount" />
