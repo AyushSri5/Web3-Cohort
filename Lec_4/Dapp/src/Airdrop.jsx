5179d8af57625b79877a777f2fc572bc5b241c97
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-09-07 11:35:09 +0530

    Token Launchpad initialize

--- Lec_4/Dapp/src/Airdrop.jsx
+++ Lec_4/Dapp/src/Airdrop.jsx
@@ -12,16 +12,33 @@ export function RequestAirdrop(){
     // console.log(LAMPORTS_PER_SOL);
     
     async function getTokens(){
-        console.log(wallet.publicKey);
-        
-        const tokens = await connection.getTokenAccountBalance(wallet.publicKey);
-        console.log(tokens);
+        if (wallet.publicKey) {
+            console.log("Public Key (Base58):", wallet.publicKey.toBase58());
+            let slot = await connection.getSlot();
+            console.log(slot);
+
+            const tokenAccounts = await connection.getParsedTokenAccountsByOwner(
+                wallet.publicKey, 
+                {
+                    programId: new solanaWeb3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+                }
+            );
+
+            tokenAccounts.value.forEach((accountInfo) => {
+                const accountData = accountInfo.account.data.parsed;
+                const tokenBalance = accountData.info.tokenAmount.uiAmount;
+                const mintAddress = accountData.info.mint;
+                console.log(`Token mint: ${mintAddress}, Balance: ${tokenBalance}`);
+            });
+        } else {
+            console.log("Wallet is not connected");
+        }
     }
     getTokens();
     async function requestAirdrop() {
         let amount = document.getElementById("amount").value;
         await connection.requestAirdrop(wallet.publicKey, amount * LAMPORTS_PER_SOL);
-        alert("Airdropped " + amount + " SOL to " + wallet.publicKey.toBase58());
+        // alert("Airdropped " + amount + " SOL to " + wallet.publicKey.toBase58());
     
         
     }
