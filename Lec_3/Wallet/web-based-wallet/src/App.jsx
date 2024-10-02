1ce4d75e394e07d7c698b02c79d8d0ac0a906c35
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-10-02 12:44:04 +0530

    Rust basics

--- Lec_3/Wallet/web-based-wallet/src/App.jsx
+++ Lec_3/Wallet/web-based-wallet/src/App.jsx
@@ -7,19 +7,29 @@ import { Keypair } from '@solana/web3.js';
 
 function App() {
   // Generate a 12-word mnemonic
+  const [publicKey, setpublicKey] = useState(null);
+  const [privateKey, setprivateKey] = useState(null);
+  const [currentIndex, setcurrentIndex] = useState(0);
+  const handleClick = () => {
   const mnemonic = generateMnemonic();
   const seed = mnemonicToSeedSync(mnemonic);
-  for (let i = 0; i < 4; i++) {
-    const path = `m/44'/501'/${i}'/0'`; // Derivation path for Solana
+  
+    const path = `m/44'/501'/${currentIndex}'/0'`; // Derivation path for Solana
     const derivedSeed = derivePath(path, seed.toString("hex")).key;
     const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
     console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
+    setpublicKey(Keypair.fromSecretKey(secret).publicKey.toBase58());
+    setprivateKey(Keypair.fromSecretKey(secret).privateKey.toBase58());
   }
-  
   
   return (
     <>
-      <h1>Wallet generation</h1>  
+      <h1>Wallet generation</h1>
+      <button onClick={handleClick}>Add Wallet</button>
+      <div>
+        <p>{publicKey}</p>
+        <p>{privateKey}</p>
+        </div>  
     </>
   )
 }
