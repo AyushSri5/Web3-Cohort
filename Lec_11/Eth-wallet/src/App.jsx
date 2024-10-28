a5a9bf988f3d81497b87270b9cd2b18ad9ac4f2c
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-10-28 21:45:33 +0530

    Solana and rust

--- Lec_11/Eth-wallet/src/App.jsx
+++ Lec_11/Eth-wallet/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from './config'
+import { WalletOptions } from '../WalletOptions'
+import { Account } from './Accounts'
+
+const queryClient = new QueryClient();
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Eth Wallet</h1>
-    </>
+    <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}> 
+      <WalletOptions />
+      <Account />
+    </QueryClientProvider> 
+  </WagmiProvider>
   )
 }
 
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,127 +1,157 @@
-// fn main() {
-//     // println!("{}",is_even(20));
-//     // println!("{}",fib(5));
-//     // let name = String::from("Hello world!");
-//     // let len = get_len(name);
-//     // println!("The length of the string is {}",len);
-//     let user = User {
-//         first_name: String::from("Harkirat"),
-//         last_name: String::from("Singh"),
-//         age: 32,
-//     };
+// // fn main() {
+// //     // println!("{}",is_even(20));
+// //     // println!("{}",fib(5));
+// //     // let name = String::from("Hello world!");
+// //     // let len = get_len(name);
+// //     // println!("The length of the string is {}",len);
+// //     let user = User {
+// //         first_name: String::from("Harkirat"),
+// //         last_name: String::from("Singh"),
+// //         age: 32,
+// //     };
 
-//     println!("{}", user.first_name);
+// //     println!("{}", user.first_name);
     
-// }
+// // }
 
-// fn is_even(num: i32) -> bool {
-//     if num % 2 == 0 {
-//         return true;
-//     } 
-//     return false;
-// }
+// // fn is_even(num: i32) -> bool {
+// //     if num % 2 == 0 {
+// //         return true;
+// //     } 
+// //     return false;
+// // }
 
-// fn fib(num: u32) -> u32 {
-//     let mut first = 0;
-//     let mut second = 1;
-//     if (num == 0){
-//         return first;
-//     }
-//     if (num ==1){
-//         return second;
-//     }
-//     for _ in 0..(num - 1) {
-//         let temp = second;
-//         second = second + first;
-//         first = temp;
-//     }
-//     return second;
-// }
+// // fn fib(num: u32) -> u32 {
+// //     let mut first = 0;
+// //     let mut second = 1;
+// //     if (num == 0){
+// //         return first;
+// //     }
+// //     if (num ==1){
+// //         return second;
+// //     }
+// //     for _ in 0..(num - 1) {
+// //         let temp = second;
+// //         second = second + first;
+// //         first = temp;
+// //     }
+// //     return second;
+// // }
 
-// fn get_len(s: String) -> usize {
-//     return s.chars().count();
-// }
+// // fn get_len(s: String) -> usize {
+// //     return s.chars().count();
+// // }
 
-// Structs
-// ----------------------------------------------------------------
+// // Structs
+// // ----------------------------------------------------------------
 
-// struct User {
-//     first_name: String,
-//     last_name: String,
-//     age: i32,
-// }
+// // struct User {
+// //     first_name: String,
+// //     last_name: String,
+// //     age: i32,
+// // }
 
 
-// ------------------------------
-// Implementation of structs
-// struct Rect {
-//     width: i32,
-//     height: i32,
-// }
+// // ------------------------------
+// // Implementation of structs
+// // struct Rect {
+// //     width: i32,
+// //     height: i32,
+// // }
 
-// impl Rect {
-//     fn area(&self) -> i32 {
-//         self.width * self.height
-//     }
+// // impl Rect {
+// //     fn area(&self) -> i32 {
+// //         self.width * self.height
+// //     }
 
-//     fn perimeter(&self, num: i32) -> i32 {
-//         2 * (self.width + self.height)
-//     }
+// //     fn perimeter(&self, num: i32) -> i32 {
+// //         2 * (self.width + self.height)
+// //     }
 
-//     fn debug() -> i32 {
-//         return 1;
-//     }
+// //     fn debug() -> i32 {
+// //         return 1;
+// //     }
+// // }
+
+// // fn main() {
+// //     let rect1 = Rect {
+// //         width: 10,
+// //         height: 20,
+// //     };
+
+// //     println!("area is {}", rect1.area());
+// //     println!("perimeter is {}", rect1.perimeter(1));
+// //     println!("debug is {}", Rect::debug());
+// // }
+
+// // Enums
+
+
+// enum Shape {
+//     Rectangle(f64, f64), // width, height
+//     Circle(f64),         // radius
 // }
 
 // fn main() {
-//     let rect1 = Rect {
-//         width: 10,
-//         height: 20,
-//     };
+//     // let rect = Shape::Rectangle(1.0, 2.0);
+//     // calculate_area(rect);
+//     // let circle = Shape::Circle(1.0);
+//     // let result = calculate_area(circle);
+//     // println!("result is {}", result);
+//     let index = first_a(String::from("Ayaush"));
 
-//     println!("area is {}", rect1.area());
-//     println!("perimeter is {}", rect1.perimeter(1));
-//     println!("debug is {}", Rect::debug());
+//     match index {
+//         Some(value) => println!("index is {}", value),
+//         None => println!("a not found"),
+//     }
 // }
 
-// Enums
+// fn calculate_area(shape: Shape) -> f64 {
+//     match shape {
+//         Shape::Rectangle(a, b) => a * b,
+//         Shape::Circle(r) => 3.14 * r * r,
+//     }
+// }
 
+// // Options 
 
-enum Shape {
-    Rectangle(f64, f64), // width, height
-    Circle(f64),         // radius
-}
+// fn first_a(s: String) -> Option<i32> {
+//     let mut i = 0;
 
+//     for(index,char) in s.chars().enumerate() {
+//         if char == 'a'{
+//             return Some(i as i32);
+//         }
+//     }
+//     return None;
+// }
+// ---------------------------------------NEW --------------------------------
 fn main() {
-    // let rect = Shape::Rectangle(1.0, 2.0);
-    // calculate_area(rect);
-    // let circle = Shape::Circle(1.0);
-    // let result = calculate_area(circle);
-    // println!("result is {}", result);
-    let index = first_a(String::from("Ayaush"));
-
-    match index {
-        Some(value) => println!("index is {}", value),
-        None => println!("a not found"),
+    println!("Hello world");
+    let index : i32 = 100;
+    let mut num: u32 = 124;
+    for i in 0..100 {
+        num += 127;
     }
-}
+    println!("Number: {}", num);
+    let is_male = false;
+    let is_above_18 = true;
+    
+    if is_male {
+        println!("You are a male");
 
-fn calculate_area(shape: Shape) -> f64 {
-    match shape {
-        Shape::Rectangle(a, b) => a * b,
-        Shape::Circle(r) => 3.14 * r * r,
+    } else {
+        println!("You are not a male");
     }
-}
 
-// Options 
-
-fn first_a(s: String) -> Option<i32> {
-    let mut i = 0;
-
-    for(index,char) in s.chars().enumerate() {
-        if char == 'a'{
-            return Some(i as i32);
-        }
+    if is_male && is_above_18 {
+        print!("You are a legal male");
     }
-    return None;
+
+    let x: &str = "ayush";
+    println!("X: {}", x);
+    let name: String = String::from("ayush");
+    let ch: Option<char> = name.chars().nth(3);
+
+    println!("{}",ch.unwrap());
 }
